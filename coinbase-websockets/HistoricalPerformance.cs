362d8244407f7038dbedf244a31ae032f5353615
danroche10 <danielroche10@gmail.com>
2023-02-27 14:45:31 +0000

    refactor: initial separation of concerns

---
+++ coinbase-websockets/HistoricalPerformance.cs
@@ -0,0 +1,37 @@
+﻿namespace backtesting
+{
+    public class HistoricalPerformance
+    {
+        public void getStrategyPerformance(string firstCoinName, string secondCoinName, Dictionary<string, float[]> pairPricingDictionary)
+        {
+
+            float gbpFunds = 1000;
+            float polygonFunds = 1000;
+            float initialFundsTotalInGBP = gbpFunds + polygonFunds * pairPricingDictionary[$"{secondCoinName} prices"].FirstOrDefault();
+
+            for (int i = 1; i < pairPricingDictionary[$"{firstCoinName} prices"].Length; i++)
+            {
+                float currentDayBitcoinPrice = pairPricingDictionary[$"{firstCoinName} prices"][i];
+                float yesterdayDayBitcoinPrice = pairPricingDictionary[$"{firstCoinName} prices"][i - 1];
+                float bitcoinPriceIncrease = currentDayBitcoinPrice / yesterdayDayBitcoinPrice;
+                if (bitcoinPriceIncrease > 1.02)
+                {
+                    gbpFunds -= 1;
+                    float currentDayPolygonPrice = pairPricingDictionary[$"{secondCoinName} prices"][i];
+                    float polygonQuanityPurchased = 1 / currentDayPolygonPrice;
+                    polygonFunds += polygonQuanityPurchased;;
+                }
+                if (bitcoinPriceIncrease < 0.98)
+                {
+                    gbpFunds += 1;
+                    float currentDayPolygonPrice = pairPricingDictionary[$"{secondCoinName} prices"][i];
+                    float polygonQuanitySold = 1 / currentDayPolygonPrice;
+                    polygonFunds -= polygonQuanitySold;
+                }
+            }
+            float finalFundsTotalInGBP = gbpFunds + polygonFunds * pairPricingDictionary[$"{secondCoinName} prices"].FirstOrDefault();
+            float changeInFunds = finalFundsTotalInGBP / initialFundsTotalInGBP;
+            float polygonPriceChange = pairPricingDictionary[$"{secondCoinName} prices"].LastOrDefault() / pairPricingDictionary[$"{secondCoinName} prices"].FirstOrDefault();
+        }
+    }
+}
---
+++ coinbase-websockets/PricingData.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace backtesting
+{
+    public class PricingData
+    {        
+        public async Task<Dictionary<string, float[]>> GetPriceComparisonDictionary(string firstCoinName, string secondCoinName, string firstCoinRequesUrl, string secondCoinRequesUrl)
+        {
+            HttpClient client = new HttpClient();
+
+            HttpResponseMessage responseForFirstCoinPriceData = await client.GetAsync(firstCoinRequesUrl);
+            HttpResponseMessage responseForSecondCoinPriceData = await client.GetAsync(secondCoinRequesUrl);
+
+            responseForFirstCoinPriceData.EnsureSuccessStatusCode();
+            responseForSecondCoinPriceData.EnsureSuccessStatusCode();
+
+            string responseForFirstCoinPriceDataContent = await responseForFirstCoinPriceData.Content.ReadAsStringAsync();
+            string responseForSecondCoinPriceDataContent = await responseForSecondCoinPriceData.Content.ReadAsStringAsync();
+
+            var firstCoinJsonObject = JObject.Parse(responseForFirstCoinPriceDataContent);
+            var firstCoinPriceDictionary = firstCoinJsonObject.ToObject<Dictionary<string, object[][]>>();
+            var secondCoinJsonObject = JObject.Parse(responseForSecondCoinPriceDataContent);
+            var secondCoinPriceDictionary = secondCoinJsonObject.ToObject<Dictionary<string, object[][]>>();
+
+            var firstCoinPrices = firstCoinPriceDictionary["prices"].Select(x => Convert.ToSingle(x[1])).ToArray();
+            var secondCoinPrices = secondCoinPriceDictionary["prices"].Select(x => Convert.ToSingle(x[1])).ToArray();
+
+            var pairPricingDictionary = new Dictionary<string, float[]>
+            {
+                { $"{firstCoinName} prices", firstCoinPrices },
+                { $"{secondCoinName} prices", secondCoinPrices }
+            };
+
+            return pairPricingDictionary;
+        }
+    }
+}
